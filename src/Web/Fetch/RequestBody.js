55b1018a3b3b0d578a10522df005270d2f017d2a
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-22 15:05:01 -0500

    Replaced 'export var' with 'export const'

--- src/Web/Fetch/RequestBody.js
+++ src/Web/Fetch/RequestBody.js
@@ -4,4 +4,4 @@ export function fromArrayBuffer(a) { return a }
 export function fromArrayView(a) { return a }
 export function fromString(a) { return a }
 export function fromReadableStream(a) { return a }
-export var empty = null;
+export const empty = null;
